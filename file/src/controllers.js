9460ff7f8b0845c0f26739e5906bfa6324b86751
Hosein KhabbazianZade <hosenkh@gmail.com>
2016-07-06 16:55:49 +0430

    new changes

--- file/src/controllers.js
+++ file/src/controllers.js
@@ -60,22 +60,124 @@
         insertBulk: {method: 'POST'}
       }
     );
-    var firstView = db.select(
-      {
-        selectionArray: [{"table": $routeParams.table, "exportFields": ["*"]}],
-        command: "select"
+    $scope.updatePageData = function () {
+      var firstView = db.select(
+        {
+          selectionArray: [{"table": $routeParams.table, "exportFields": ["*"]}],
+          command: "select"
+        }
+      ),
+      gotString = '';
+      firstView.$promise.then(function(data){
+        for (var i in data) {
+          if (typeof(data[i]) == 'string') {
+            gotString += data[i];
+          }
+        }
+        gotString = gotString.slice(1);
+        $scope.mainTable = JSON.parse(gotString);
+        $scope.tableWidth = _.keys($scope.mainTable[0]);
+        $scope.setWidth = function () {
+          return {"width": $scope.tableWidth.length*20+'%'};
+        };
+        $scope.edittedArray = [[]];
+        for (i in $scope.tableWidth) {
+          $scope.edittedArray[0].push({key: $scope.tableWidth[i], value: ''});
+        }
+        $scope.edittedArray[0].shift();
+      });
+    };
+    $scope.updatePageData();
+    $scope.newRowActive = function () {
+      $scope.newRow = true;
+      if ($scope.edittedArray.length <1) {
+        $scope.edittedArray = [[]];
+        for (var i in $scope.tableWidth) {
+          $scope.edittedArray[0].push({key: $scope.tableWidth[i], value: ''});
+        }
+        $scope.edittedArray[0].shift();
       }
-    ),
-    gotString = '';
-    firstView.$promise.then(function(data){
-      for (var i in data) {
-        if (typeof(data[i]) == 'string') {
-          gotString += data[i];
+    };
+    $scope.moreNewRows = function () {
+      $scope.edittedArray.push([]);
+      for (var i in $scope.tableWidth) {
+        $scope.edittedArray[$scope.edittedArray.length-1].push({key: $scope.tableWidth[i], value: ''});
+      }
+      $scope.edittedArray[$scope.edittedArray.length-1].shift();
+      console.log($scope.edittedArray);
+    };
+    $scope.addRow = function () {
+      var data = {};
+      for (var i in $scope.edittedArray[0]) {
+        data[$scope.edittedArray[0][i].key] = $scope.edittedArray[0][i].value;
+      }
+      db.insert(
+        {
+          command: "insert",
+          data: data,
+          selectionArray: $routeParams.table
+        }
+      );
+      $scope.newRow = false;
+      for (i in $scope.edittedArray[0]) {
+        $scope.edittedArray[0][i].value = '';
+      }
+      $scope.updatePageData();
+    };
+    $scope.sendAll = function () {
+      for (var i in $scope.edittedArray) {
+        isEmpty = true;
+        for (var j in $scope.edittedArray[i]) {
+          isEmpty = isEmpty && $scope.edittedArray[i][j].value === '';
+        }
+        if (isEmpty === true) {
+          $scope.edittedArray.splice(i,1);
         }
       }
-      gotString = gotString.slice(1);
-      $scope.mainTable = JSON.parse(gotString);
-    });
+      var columns = JSON.parse(JSON.stringify($scope.tableWidth));
+      columns.shift();
+      var data = {columns: columns, data:[]};
+      for (i in $scope.edittedArray) {
+        data.data.push([]);
+        for (j in $scope.edittedArray[i]) {
+          data.data[data.data.length-1].push($scope.edittedArray[i][j].value);
+        }
+        data.data[i].pop();
+      }
+      console.log(data);
+      db.insertBulk(
+        {
+          command: 'insertBulk',
+          selectionArray: $routeParams.table,
+          data: data
+        }
+      );
+      $scope.newRow = false;
+      for (j in $scope.edittedArray) {
+        for (i in $scope.edittedArray[j]) {
+          $scope.edittedArray[0][j].value = '';
+        }  
+      }
+      $scope.updatePageData();
+    };
+    $scope.dropNewRow = function (array) {
+      for (var i in $scope.edittedArray) {
+        if ($scope.edittedArray[i] == array) {
+          $scope.edittedArray.splice(i,1);
+          return;
+        }
+      }
+      if ($scope.edittedArray.length < 2) {
+        $scope.newRow = false;
+      }
+    };
+
+    $scope.deleteClick = function () {
+
+    };
+    $scope.deleteRow = function () {
+
+    };
   },
 
   /**
